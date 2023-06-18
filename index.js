5d2a58e0ec91bac7d46c03c9029f85929b8fbea4
rahyunjae <rahyun222@naver.com>
2023-06-18 22:10:20 +0900

    Commit message

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
-const {User} = require("./modls/User")
-const bodyParser = require("body-parser")
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require('./config/key');
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-//application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-
-//application/json
+// application/json
 app.use(bodyParser.json());
 
-
-
-
-mongoose.connect('mongodb+srv://rahyun:112288ra@cluster0.ip2razi.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.post('/register', (req, res) => {
+  res.send('새해 복 많이 받으세요!');
+});
 
-  const user = new User(req.body) // req.body로 유저 정보를 가져오는 것 -> DB에 넣어야 한다.
-  //model 틀에 body -> 물을 얼음통에 넣음 -> 동그란 얼음: 물 body, 얼음통 User, 동그란 얼음 user
-  
-  user.save((err,userInfo)=>{
-    if(err) return res.json({success:false,err})
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
     return res.status(200).json({
-      success:true
-}
+      success: true
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
